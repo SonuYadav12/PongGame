ecef72bad395540fe016b52d9db2e74e62a39e67
Sonu <yadavsonukumar036@gmail.com>
2023-03-26 11:42:03 +0530

    .

---
+++ GamePanel.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GamePanel
+{
+GamePanel()
+{
+
+}
+public void newBall()
+{
+
+}
+
+public void NewPaddles()
+{
+
+}
+public void paint(Graphics g)
+{
+
+}
+public void draw(Graphics g)
+{
+
+}
+public void move()
+{
+
+}
+public void checkCollision()
+{
+
+}
+public void run()
+{
+
+}
+public class AL extends KeyAdapter{
+    public void KeyPressed(KeyEvent e)
+    {
+
+    }
+    public void KeyReleased(KeyEvent e)
+    {
+
+    }
+}
+
+}
---
+++ PongGame.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PongGame {
+    PongGame(){
+        
+    }
+}
